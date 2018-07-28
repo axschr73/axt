9499097d3cf8da734f353a6a97011a09dfc1b06b
Axel Schroeder <axel@randall.localdomain>
2018-07-28 19:03:40 +0200

    axt.sh 1st version

---
+++ axt.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+# setup environment
+cd $(dirname $0)
+export AXT_PATH=$(pwd)
+
+source ${AXT_PATH}/.axtrc
+
+
+# process subcommand
+SUBCOMMAND=$1
+shift
+
+if [[ -z $SUBCOMMAND ]]; then
+    SUBCOMMAND="help"
+fi
+
+
+# execute full command
+COMMAND="axt-${SUBCOMMAND}.sh"
+FULL_COMMAND=$(find ${AXTPATH} -name ${COMMAND} -print)
+
+if [[ ${FULL_COMMAND} ]]; then
+	eval ${FULL_COMMAND}
+else
+	echo "axt: Unknown command '${SUBCOMMAND}'"
+fi
+
