28eedbcbada2a8f885652af69985efb449a26a5c
Axel Schroeder <axel.schroeder@sap.com>
2018-08-28 13:33:56 +0200

    various fixes

--- axt-git-remote.sh
+++ axt-git-remote.sh
@@ -10,7 +10,7 @@ if [[ ! -v AXT_PATH ]] || [ -z "${AXT_PATH}" ]; then
 	echo >&2 "AXT ERROR: Please execute $0 from axt"
 	exit
 fi
-command -v peco  >/dev/null 2>&1 || { echo >&2 "AXT ERROR: axt git remote requires helper 'peco'!"; exit }
+command -v peco  >/dev/null 2>&1 || { echo >&2 "AXT ERROR: axt git remote requires helper 'peco'!"; exit; }
 
 
 if (( $# <= 1 )); then
